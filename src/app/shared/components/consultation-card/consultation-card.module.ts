3b90080fda1cbf4bcc66830467c76086061050b4
barath <barath@commutatus.com>
2019-07-01 13:30:59 +0530

    chore: Landing and consultation-card components created

---
+++ src/app/shared/components/consultation-card/consultation-card.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConsultationCardComponent } from './consultation-card.component';
+
+@NgModule({
+  declarations: [ConsultationCardComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConsultationCardModule { }

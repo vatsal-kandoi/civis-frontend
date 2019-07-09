a2a5a171af24f88ddc30ac97883cc12d7f55e70c
Nishanthh <nishanth@commutatus.com>
2019-07-09 18:04:37 +0530

    fix: Removed Unused Component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import {NgSelectModule} from '@ng-select/ng-select';
 import { FormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
@@ -11,7 +10,6 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
-        AuthComponent,
         SignUpComponent,
         LoginComponent
     ],

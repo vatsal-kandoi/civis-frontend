8a17ba7806cff0accbc7cd9d86baa08cc2bb645d
barath <barath@commutatus.com>
2019-08-06 18:59:08 +0530

    feat: Content policy page

--- src/app/modules/policy/policy.module.ts
+++ src/app/modules/policy/policy.module.ts
@@ -3,16 +3,21 @@ import { CommonModule } from '@angular/common';
 import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 import { SharedComponentsModule } from 'src/app/shared/components/shared-components.module';
 import { Routes, RouterModule } from '@angular/router';
+import { ContentPolicyComponent } from './content-policy/content-policy.component';
 
 const routes: Routes = [
   {
     path: 'terms-of-service',
     component: TermsOfServiceComponent
+  },
+  {
+    path: 'content-policy',
+    component: ContentPolicyComponent
   }
 ];
 
 @NgModule({
-  declarations: [TermsOfServiceComponent],
+  declarations: [TermsOfServiceComponent, ContentPolicyComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),

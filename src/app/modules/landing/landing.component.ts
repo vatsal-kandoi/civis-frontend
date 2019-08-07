1ba95a0edf25e0ef0bfb3a8cfbf612e10b86f0e7
barath <barath@commutatus.com>
2019-08-07 16:21:38 +0530

    feat: In focus card carousel

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -1,56 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs/Rx';
+import { map } from 'rxjs/operators';
+import { slideInRight } from '../../shared/animations/slide';
+import { ConsultationList } from '../consultations/consultation-list/consultation-list.graphql';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  animations: [slideInRight],
 })
+
 export class LandingComponent implements OnInit {
-  covercardData: any;
-  cardData = [
-    {
-      ministry: {
-        name: 'Ministry of Housing & Urban Affairs',
-        coverPhoto: {
-          url: 'http://sf.co.ua/14/09/wallpaper-1341205.jpg'
-        }
-      },
-      title: 'National Urban Policy Framework, 2018',
-      days: '12 Days Remaining',
-      response: '6,712 Responses so far' 
-    },
+  coverCardData: any;
+  current_index = 0;
 
-    {
-      ministry: {
-        name: 'Ministry of Housing & Urban Affairs',
-        coverPhoto: {
-          url: 'http://sf.co.ua/14/09/wallpaper-1341205.jpg'
-        }
-      },
-      title: 'National Urban Policy Framework, 2018',
-      days: '12 Days Remaining',
-      response: '6,712 Responses so far' 
-    },
-
-    {
-       ministry: {
-        name: 'Ministry of Housing & Urban Affairs',
-        coverPhoto: {
-          url: 'http://sf.co.ua/14/09/wallpaper-1341205.jpg'
-        }
-      },
-      title: 'National Urban Policy Framework, 2018',
-      days: '12 Days Remaining',
-      response: '6,712 Responses so far' 
-    }
-  ]
-  constructor() { }
+constructor( private apollo: Apollo ) { }
   
   ngOnInit() {
-    this.covercardData = this.cardData[0];
+    this.getConsultationCard();
+    this.rotateFeature();
   }
   
-  showCard(index) {
-    this.covercardData = this.cardData[index];
+  getConsultationCard() {
+    const variables = {
+      perPage: null,
+      page: null,
+      statusFilter: 'published'
+    };
+    this.apollo.query({
+      query: ConsultationList, 
+      variables: variables
+    })
+    .pipe (
+      map((res: any) => res.data.consultationList)
+    )
+    .subscribe((item: any) => {
+        this.coverCardData = item.data;
+    }, err => {
+      console.log('err', err);
+    });
   }
+  
+  rotateFeature() {
+    Observable.interval(5000).subscribe(() => {
+      if (this.current_index === 2) {
+        this.current_index = 0;
+      } else {
+        this.current_index++;
+      }
+    });
+  }
+
+  changeCard(index) {
+    this.current_index = index;
+  }
+
 }

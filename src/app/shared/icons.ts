8d316cb7ee25dfc121870e26e26d43642bcb5a73
Nishanthh <32632127+Nishanthh@users.noreply.github.com>
2019-08-26 18:41:17 +0530

    feat: Share consultation (#41)
    * feat: added share consultation process
    
    * feat: consultation link as a shareable link
    
    * feat: click outside close popup

---
+++ src/app/shared/icons.ts
@@ -0,0 +1,11 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+
+
+const icons = [
+  faFacebookF, faTwitter
+];
+
+library.add(...icons);

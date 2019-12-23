71a46fc0a0828944281cccab9f4318699e4dbb9b
Nishanthh <nishanth@commutatus.com>
2019-12-23 13:31:43 +0530

    feat: Added scroll to response from profile modal

--- src/app/shared/user-profile-modal/user-profile-modal.component.ts
+++ src/app/shared/user-profile-modal/user-profile-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
 import { ModalDirective } from 'ngx-bootstrap';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile-modal',
@@ -11,15 +13,33 @@ export class UserProfileModalComponent implements OnInit {
   @Input() leaderData: any;
   @Output() close: EventEmitter<any> = new EventEmitter();
   @ViewChild('leaderModal', { static: false }) leaderModal: ModalDirective;
+  currentUser: any;
 
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.getCurrentUser();
   }
+
   closeModal() {
     this.leaderModal.hide();
     this.close.emit(true);
+  }
+
+  getCurrentUser() {
+    this.userService.userLoaded$
+      .subscribe((data) => {
+        if (data) {
+          this.currentUser = this.userService.currentUser;
+        } else {
+          this.currentUser = null;
+        }
+      });
+  }
+
+  viewResponse(response) {
+    this.router.navigateByUrl(`/consultations/${response.consultation.id}/read#${response.id}`);
   }
 
 }

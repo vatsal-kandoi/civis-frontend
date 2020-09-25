977a0278c5673e7ead15822c9237976f36d79b4d
Nishanthh <nishanth@commutatus.com>
2020-09-25 14:21:31 +0530

    feat: including tabs in public responses

--- src/app/modules/consultations/consultation-profile/consultation-public-responses/consultation-public-responses.component.ts
+++ src/app/modules/consultations/consultation-profile/consultation-public-responses/consultation-public-responses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewChecked, ViewChild, ElementRef } from '@angular/core';
 import { ConsultationsService } from 'src/app/shared/services/consultations.service';
 import { ActivatedRoute } from '@angular/router';
+import { isObjectEmpty } from 'src/app/shared/functions/modular.functions';
 
 @Component({
   selector: 'app-consultation-public-responses',
@@ -17,6 +18,9 @@ export class ConsultationPublicResponsesComponent implements OnInit, AfterViewCh
   showUserProfileModal: boolean;
   fragment: any;
   checkForFragments: any;
+  roundNumber: any;
+  responseRounds: any;
+  publicResponsesLength: any;
 
   constructor(private consultationService: ConsultationsService, private route: ActivatedRoute) { }
 
@@ -36,7 +40,10 @@ export class ConsultationPublicResponsesComponent implements OnInit, AfterViewCh
     this.consultationService.consultationProfileData.subscribe((data) => {
       if (data) {
         this.profileData = data;
+        this.roundNumber = this.getActiveRound(data);
+        this.responseRounds = this.profileData.responseRounds;
         this.responseList = data.sharedResponses.edges;
+        this.publicResponsesLength = this.responseList.filter((response) => response.node.roundNumber === this.roundNumber).length;
         this.checkForFragments = true;
       }
     });
@@ -73,6 +80,24 @@ export class ConsultationPublicResponsesComponent implements OnInit, AfterViewCh
         this.consultationService.scrollToPublicResponse.next(false);
       }
     });
+  }
+
+  getActiveRound(profileData) {
+    if (profileData && profileData.responseRounds) {
+      const responseRounds = profileData.responseRounds;
+      if (responseRounds && responseRounds.length) {
+        const activeRound  = responseRounds.find((round) => round.active);
+        if (!isObjectEmpty(activeRound)) {
+          return activeRound.roundNumber;
+        }
+      }
+    }
+    return;
+  }
+
+  setActiveRound(roundNumber) {
+    this.roundNumber = roundNumber;
+    this.publicResponsesLength = this.responseList.filter((response) => response.node.roundNumber === this.roundNumber).length;
   }
 
 }

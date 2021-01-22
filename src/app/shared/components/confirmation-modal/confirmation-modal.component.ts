67d315842b528b3ec1ee3a7ade867e79dfb50273
Harshwardhan Pauranik <53950319+Harshwardhan18@users.noreply.github.com>
2021-01-22 12:08:29 +0530

    feat: lang opt on page load (#196)
    * feat: lang opt on page load
    
    * feat: added confirmation modal

--- src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
+++ src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-confirmation-modal',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirmation-modal.component.scss']
 })
 export class ConfirmationModalComponent implements OnInit {
-
+  @Input() message = {
+    title: null,
+    msg: null
+  };
+  @Output() close = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  selectOpt(opt: string) {
+    opt === 'ok' ? this.close.emit(true) : this.close.emit(false);
   }
 
 }

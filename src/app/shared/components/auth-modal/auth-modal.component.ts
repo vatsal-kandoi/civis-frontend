b3be418358271c702eb2d6897749afd2c82efc08
preethijayanthi <preethi8895@gmail.com>
2021-02-19 17:29:14 +0530

    fix: Fixed the validation issue (#207)

--- src/app/shared/components/auth-modal/auth-modal.component.ts
+++ src/app/shared/components/auth-modal/auth-modal.component.ts
@@ -67,7 +67,7 @@ export class AuthModalComponent implements OnInit {
       const variables: any = {
         auth: signupObject
       };
-      variables.auth.referringConsultationId  = this.consultationId;
+      variables.auth.referring_consultation_id  = this.consultationId;
       this.apollo.mutate({mutation: SignUpMutation, variables: variables})
       .pipe(
         map((res: any) => res.data.authSignUp)

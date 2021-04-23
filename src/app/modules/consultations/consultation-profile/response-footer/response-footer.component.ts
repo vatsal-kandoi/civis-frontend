300df6b37cc55a338caa377aa03fb36a734393d8
Harshwardhan <harshw.pauranik@gmail.com>
2021-04-23 16:23:12 +0530

    impr: draft and user response without login

--- src/app/modules/consultations/consultation-profile/response-footer/response-footer.component.ts
+++ src/app/modules/consultations/consultation-profile/response-footer/response-footer.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, HostListener } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  HostListener,
+} from '@angular/core';
 import { UserService } from 'src/app/shared/services/user.service';
 import { ErrorService } from 'src/app/shared/components/error-modal/error.service';
 import { Apollo } from 'apollo-angular';
-import { VoteDeleteQuery, ConsultationProfileCurrentUser, VoteCreateQuery } from '../consultation-profile.graphql';
+import {
+  VoteDeleteQuery,
+  ConsultationProfileCurrentUser,
+  VoteCreateQuery,
+} from '../consultation-profile.graphql';
 import { ConsultationsService } from 'src/app/shared/services/consultations.service';
 import { filter } from 'rxjs/operators';
 import * as moment from 'moment';
 import { getSocialLink } from '../../consultation-profile/socialLink.function';
 import { isObjectEmpty } from 'src/app/shared/functions/modular.functions';
 
-
 @Component({
   selector: 'app-response-footer',
   templateUrl: './response-footer.component.html',
-  styleUrls: ['./response-footer.component.scss']
+  styleUrls: ['./response-footer.component.scss'],
 })
 export class ResponseFooterComponent implements OnInit {
-
-  @ViewChild('shareBlockElement', { static: false }) shareBlockElement: ElementRef;
-  @ViewChild('shareButtonElement', { static: false }) shareButtonElement: ElementRef;
+  @ViewChild('shareBlockElement', { static: false })
+  shareBlockElement: ElementRef;
+  @ViewChild('shareButtonElement', { static: false })
+  shareButtonElement: ElementRef;
 
   @Input() response;
   currentUser: any;
@@ -30,23 +41,23 @@ export class ResponseFooterComponent implements OnInit {
   responseQuestions: any;
   longTextResponses: any;
   shareBtnClicked: any;
-  getSocialLink  = getSocialLink;
+  getSocialLink = getSocialLink;
   currentUrl: string;
   activeRoundNumber: any;
   responseRounds: any;
 
-  constructor(private userService: UserService,
-              private errorService: ErrorService,
-              private apollo: Apollo,
-              private consultationService: ConsultationsService) {
-                this.consultationService.consultationId$
-                .pipe(
-                  filter(i => i !== null)
-                )
-                .subscribe((consulationId: any) => {
-                  this.consultationId = consulationId;
-                });
-               }
+  constructor(
+    private userService: UserService,
+    private errorService: ErrorService,
+    private apollo: Apollo,
+    private consultationService: ConsultationsService
+  ) {
+    this.consultationService.consultationId$
+      .pipe(filter((i) => i !== null))
+      .subscribe((consulationId: any) => {
+        this.consultationId = consulationId;
+      });
+  }
 
   ngOnInit(): void {
     this.currentUrl = window.location.href;
@@ -62,7 +73,7 @@ export class ResponseFooterComponent implements OnInit {
         return;
       }
       if (this.shareBlockElement.nativeElement.contains(targetElement)) {
-            return;
+        return;
       } else {
         this.showShareBlock = false;
       }
@@ -70,8 +81,7 @@ export class ResponseFooterComponent implements OnInit {
   }
 
   getCurrentUser() {
-    this.userService.userLoaded$
-    .subscribe((data) => {
+    this.userService.userLoaded$.subscribe((data) => {
       if (data) {
         this.currentUser = this.userService.currentUser;
       } else {
@@ -102,36 +112,50 @@ export class ResponseFooterComponent implements OnInit {
 
   undoVote(response, direction, createVote?) {
     if (response.id) {
-      this.apollo.mutate({
-        mutation: VoteDeleteQuery,
-        variables: {
-          consultationResponseId: response.id
-        },
-        update: (store, {data: res}) => {
-          const variables = {id: this.consultationId};
-          const resp: any = store.readQuery({query: ConsultationProfileCurrentUser, variables});
-          if (res) {
-            for (const value of resp['consultationProfile'].sharedResponses.edges) {
-              if (value.node.id ===  response['id']) {
-                if (response.votedAs) {
-                  value.node[response.votedAs.voteDirection + 'VoteCount'] -= 1;
+      this.apollo
+        .mutate({
+          mutation: VoteDeleteQuery,
+          variables: {
+            consultationResponseId: response.id,
+          },
+          update: (store, { data: res }) => {
+            const variables = { id: this.consultationId };
+            const resp: any = store.readQuery({
+              query: ConsultationProfileCurrentUser,
+              variables,
+            });
+            if (res) {
+              for (const value of resp['consultationProfile'].sharedResponses
+                .edges) {
+                if (value.node.id === response['id']) {
+                  if (response.votedAs) {
+                    value.node[
+                      response.votedAs.voteDirection + 'VoteCount'
+                    ] -= 1;
+                  }
+                  value.node.votedAs = null;
+                  this.response = value;
+                  break;
                 }
-                value.node.votedAs = null;
-                this.response = value;
-                break;
               }
             }
+            store.writeQuery({
+              query: ConsultationProfileCurrentUser,
+              variables,
+              data: resp,
+            });
+          },
+        })
+        .subscribe(
+          (res) => {
+            if (createVote) {
+              this.createVote(response, direction);
+            }
+          },
+          (err) => {
+            this.errorService.showErrorModal(err);
           }
-          store.writeQuery({query: ConsultationProfileCurrentUser, variables, data: resp});
-        }
-      })
-      .subscribe((res) => {
-        if (createVote) {
-          this.createVote(response, direction);
-        }
-      }, err => {
-        this.errorService.showErrorModal(err);
-      });
+        );
     }
   }
 
@@ -139,36 +163,47 @@ export class ResponseFooterComponent implements OnInit {
     const vote = {
       consultationResponseVote: {
         consultationResponseId: response.id,
-        voteDirection: direction
-      }
+        voteDirection: direction,
+      },
     };
-    this.apollo.mutate({
-      mutation: VoteCreateQuery,
-      variables: vote,
-      update: (store, {data: res}) => {
-        const variables = {id: this.consultationId};
-        const resp: any = store.readQuery({query: ConsultationProfileCurrentUser, variables});
-        if (res) {
-          for (const value of resp['consultationProfile'].sharedResponses.edges) {
-            if (value.node.id ===  response['id']) {
-              if (value.node[res.voteCreate.voteDirection + 'VoteCount']) {
-                value.node[res.voteCreate.voteDirection + 'VoteCount'] += 1;
-              } else {
-                value.node[res.voteCreate.voteDirection + 'VoteCount'] = 1;
+    this.apollo
+      .mutate({
+        mutation: VoteCreateQuery,
+        variables: vote,
+        update: (store, { data: res }) => {
+          const variables = { id: this.consultationId };
+          const resp: any = store.readQuery({
+            query: ConsultationProfileCurrentUser,
+            variables,
+          });
+          if (res) {
+            for (const value of resp['consultationProfile'].sharedResponses
+              .edges) {
+              if (value.node.id === response['id']) {
+                if (value.node[res.voteCreate.voteDirection + 'VoteCount']) {
+                  value.node[res.voteCreate.voteDirection + 'VoteCount'] += 1;
+                } else {
+                  value.node[res.voteCreate.voteDirection + 'VoteCount'] = 1;
+                }
+                value.node.votedAs = res.voteCreate;
+                this.response = value;
+                break;
               }
-              value.node.votedAs = res.voteCreate;
-              this.response = value;
-              break;
             }
           }
+          store.writeQuery({
+            query: ConsultationProfileCurrentUser,
+            variables,
+            data: resp,
+          });
+        },
+      })
+      .subscribe(
+        (data) => {},
+        (err) => {
+          this.errorService.showErrorModal(err);
         }
-        store.writeQuery({query: ConsultationProfileCurrentUser, variables, data: resp});
-      }
-    })
-    .subscribe((data) => {
-    }, err => {
-      this.errorService.showErrorModal(err);
-    });
+      );
   }
 
   toggleShareBlock(id) {
@@ -179,9 +214,13 @@ export class ResponseFooterComponent implements OnInit {
   }
 
   showCreateResponse() {
-    if ((this.consultationService.checkClosed(this.profileData ? this.profileData.responseDeadline : null) === 'Closed')
-        || !this.currentUser || (this.profileData && this.profileData.respondedOn)) {
-        return false;
+    if (
+      this.consultationService.checkClosed(
+        this.profileData ? this.profileData.responseDeadline : null
+      ) === 'Closed' ||
+      (this.profileData && this.profileData.respondedOn)
+    ) {
+      return false;
     }
     return true;
   }
@@ -192,20 +231,23 @@ export class ResponseFooterComponent implements OnInit {
     }
     if (response) {
       this.usingTemplate = true;
-      this.responseQuestions = this.consultationService.getQuestions(this.profileData, response.roundNumber);
+      this.responseQuestions = this.consultationService.getQuestions(
+        this.profileData,
+        response.roundNumber
+      );
       if (this.responseQuestions && this.responseQuestions.length) {
         this.longTextResponses = this.getLongTextAnswer(response);
         if (this.longTextResponses && this.longTextResponses.length) {
           const obj = {
             longTextResponses: this.longTextResponses,
-            templateId: response.id
+            templateId: response.id,
           };
           this.consultationService.useThisResponseAnswer.next(obj);
         }
       } else {
         const obj = {
           responseText: response.responseText,
-          templateId: response.id
+          templateId: response.id,
         };
         this.consultationService.useThisResponseText.next(obj);
       }
@@ -218,13 +260,15 @@ export class ResponseFooterComponent implements OnInit {
     if (responseAnswers && responseAnswers.length) {
       responseAnswers.map((item) => {
         if (this.responseQuestions && this.responseQuestions.length) {
-          const responseQuestion = this.responseQuestions.find((question) => +question.id === +item.question_id);
+          const responseQuestion = this.responseQuestions.find(
+            (question) => +question.id === +item.question_id
+          );
           if (responseQuestion.questionType === 'long_text') {
             answers.push({
               id: responseQuestion.id,
               questionType: responseQuestion.questionType,
               questionText: responseQuestion.questionText,
-              answer: item.answer
+              answer: item.answer,
             });
           }
         }
@@ -236,13 +280,11 @@ export class ResponseFooterComponent implements OnInit {
 
   getActiveRound(responseRounds) {
     if (responseRounds && responseRounds.length) {
-      const activeRound  = responseRounds.find((round) => round.active);
+      const activeRound = responseRounds.find((round) => round.active);
       if (!isObjectEmpty(activeRound)) {
         return activeRound.roundNumber;
       }
     }
-  return;
+    return;
+  }
 }
-
-}
-

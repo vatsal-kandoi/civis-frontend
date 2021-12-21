cb0a4bfaf20536aac519346fa8da6f476b6fc0ad
manzurkds <manzurkds@gmail.com>
2021-12-21 15:50:57 +0530

    chore: Removed jp morgan api url

--- src/environments/environment.ffg.staging.ts
+++
@@ -1,9 +0,0 @@
-import {environment as prodEnv} from './environment.prod';
-
-export const environment = {
-    ...prodEnv,
-    staging: true,
-    production: false,
-    api: 'https://civis-api-jpmc-backend.staging-9143.c66.me',
-    RECAPTCHA_SITE_KEY: '6Ld8GLUUAAAAAH5CZbqDdQDwl-s5ZC2ZqHz5TWyj'
-};
